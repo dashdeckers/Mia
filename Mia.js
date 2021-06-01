6f44a6ab027cbb9b6060642f0b69b5f59b1c1d2d
Travis Hammond <dashdeckers@gmail.com>
2021-06-01 13:48:59 +0200

    Define probabilities for each possible roll

--- Mia.js
+++ Mia.js
@@ -10,8 +10,6 @@ const roll_die = () => {
 // Doubles are always higher than normal rolls.
 // The "Mia" (1 and 2) is a special roll and has the highest value.
 const value_order = [
-    // all double rolls have a probability of 1/36
-    // all other rolls have a probability of 2/36
     31, 32,
     41, 42, 43,
     51, 52, 53, 54,
@@ -19,6 +17,12 @@ const value_order = [
     11, 22, 33, 44, 55, 66,
     12,
 ]
+// This is an array containing the probabilities corresponding to the value_order array
+const roll_probs = Array(21).fill().map(
+    // all double rolls have a probability of 1/36
+    // all other rolls have a probability of 2/36
+    (_, idx) => {return idx < 14 || idx == 20 ? 2/36 : 1/36}
+)
 
 
 const Player = (name) => {
